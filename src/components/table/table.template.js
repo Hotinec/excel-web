722f9446d132d733269de5ead6a3689ba3420cf3
Hotinec Artiom <hotinec2@gmail.com>
2020-05-25 11:33:52 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,22 +4,30 @@ const CODES = {
   Z: 90
 }
 
-function toCells(cell) {
+function toCells(_, col) {
   return `
-    <div class="cell" contenteditable>${cell}</div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+     </div>
   `
 }
 
 function createRow(index, content) {
+  const resizer = index ? '<div class="row-resize" data-resize="row"></div>'
+                        : ''
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resizer}
+      </div>
       <div class="row-data">${content}</div>
      </div>
   `
